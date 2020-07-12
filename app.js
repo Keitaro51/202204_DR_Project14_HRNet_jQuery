b1213295b0a8ba9d0da3672c98ba99445904267e
Michael Angerville <michael.angerville@outlook.com>
2020-07-12 00:42:41 -0400

    Replacing with plain old HTML JS, CSS, and jQuery app

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
-const US_STATES = [
+$( function() {
+    const stateSelect = document.getElementById('state');
+    states.forEach(function(state) {
+        const option = document.createElement('option');
+        option.value = state.abbreviation;
+        option.text = state.name;
+        stateSelect.appendChild(option);
+    });
+
+    $( "#department" ).selectmenu();
+    $( "#state" ).selectmenu();
+
+    $('#date-of-birth').datetimepicker({
+        timepicker: false,
+        format: 'm/d/Y'
+    });
+    $('#start-date').datetimepicker({
+        timepicker: false,
+        format: 'm/d/Y'
+    });
+});
+
+function saveEmployee() {
+    const firstName = document.getElementById('first-name');
+    const lastName = document.getElementById('last-name');
+    const dateOfBirth = document.getElementById('date-of-birth');
+    const startDate = document.getElementById('start-date');
+    const department = document.getElementById('department');
+    const street = document.getElementById('street');
+    const city = document.getElementById('city');
+    const state = document.getElementById('state');
+    const zipCode = document.getElementById('zip-code');
+
+    const employees = JSON.parse(localStorage.getItem('employees')) || [];
+    const employee = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        dateOfBirth: dateOfBirth.value,
+        startDate: startDate.value,
+        department: department.value,
+        street: street.value,
+        city: city.value,
+        state: state.value,
+        zipCode: zipCode.value
+    };
+    employees.push(employee);
+    localStorage.setItem('employees', JSON.stringify(employees));
+    $('#confirmation').modal();
+}
+
+const states = [
     {
         "name": "Alabama",
         "abbreviation": "AL"
@@ -236,5 +286,3 @@ const US_STATES = [
         "abbreviation": "WY"
     }
 ];
-
-export default US_STATES;
--- src/stores/CreateEmployeeStore/index.js
+++
@@ -1,4 +0,0 @@
-import CreateEmployeeStore from './CreateEmployeeStore';
-
-// create singleton interface
-export default new CreateEmployeeStore();
